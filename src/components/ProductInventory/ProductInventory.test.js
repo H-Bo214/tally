51c01762c90752941ac263406cab7b7e511de3ee
Horacio Borrego <hborrego365@gmail.com>
2023-01-09 13:02:39 -0800

    Add tests for ProductInventory to confirm the render of product headings, error message and product list.

--- src/components/ProductInventory/ProductInventory.test.js
+++ src/components/ProductInventory/ProductInventory.test.js
@@ -1,7 +1,22 @@
 import ProductInventory from './ProductInventory'
 import { render, screen } from '@testing-library/react'
+import { products } from '../../testData'
 
 describe('ProductInventory', () => {
+  it('should render product headings', () => {
+    render(<ProductInventory />)
+    const prod = screen.getByText('Product')
+    const description = screen.getByText('Description')
+    const price = screen.getByText('Price')
+    const quantity = screen.getByText('Quantity')
+    const status = screen.getByText('Status')
+    expect(prod).toBeInTheDocument()
+    expect(description).toBeInTheDocument()
+    expect(price).toBeInTheDocument()
+    expect(quantity).toBeInTheDocument()
+    expect(status).toBeInTheDocument()
+  })
+
   it('should render an error message if product fetching fails', () => {
     const emptyProducts = []
     const error =
@@ -9,5 +24,10 @@ describe('ProductInventory', () => {
     render(<ProductInventory products={emptyProducts} error={error} />)
     const message = screen.getByText(error)
     expect(message).toBeInTheDocument()
+  })
+
+  it('should render a list of products when data is provided.', () => {
+    render(<ProductInventory products={products} />)
+    expect(products).toHaveLength(3)
   })
 })
