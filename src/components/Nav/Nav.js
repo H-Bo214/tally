5a8d267b8b2e1e5495e3fb33d4afc284582bf93a
Horacio Borrego <hborrego365@gmail.com>
2023-02-22 11:09:34 -0800

    Remove child styling, add style to new parent

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,19 +4,16 @@ import tallyLogo from '../../assets/tally-logo.svg'
 const Nav = () => {
   return (
     <nav className='nav'>
-      <section className='title-container'>
-        <img
-          src={tallyLogo}
-          alt='Tally inventory tracker logo'
-          width='131'
-          height='45'
-        />
-      </section>
-      <ul className='nav-list'>
-        <li className='inventory'>Inventory</li>
-        <li className='orders'>Orders</li>
-        <li className='customers'>Customers</li>
-      </ul>
+      <div className='nav-children-container'>
+        <section className='title-container'>
+          <img src={tallyLogo} alt='Tally inventory tracker logo' />
+        </section>
+        <ul className='nav-list'>
+          <li className='inventory'>Inventory</li>
+          <li className='orders'>Orders</li>
+          <li className='customers'>Customers</li>
+        </ul>
+      </div>
     </nav>
   )
 }
