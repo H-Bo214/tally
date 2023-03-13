288641ae4ab4a42e6180c7892c4d050c3086757d
Horacio Borrego <hborrego365@gmail.com>
2023-03-13 12:35:36 -0700

    Create ConfirmationModal test to check for prompt

--- src/components/ConfirmationModal/ConfirmationModal.test.js
+++ src/components/ConfirmationModal/ConfirmationModal.test.js
@@ -8,4 +8,14 @@ describe('ConfirmationModal', () => {
     const h3 = screen.getByRole('heading', { name: /Save your edits?/i })
     expect(h3).toBeInTheDocument()
   })
+
+  it('should contain a prompt', () => {
+    render(
+      <ConfirmationModal prompt='Your changes to this product will be lost if not saved' />
+    )
+    const text = screen.getByText(
+      'Your changes to this product will be lost if not saved'
+    )
+    expect(text).toBeInTheDocument()
+  })
 })
