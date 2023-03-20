36d934ef686de31f0a364a6fdfc6b1499c3adb41
Horacio Borrego <hborrego365@gmail.com>
2023-03-20 14:35:32 -0700

    Add integration tests to confirm presence of a single and multiple products on page load.

--- src/components/MainSectionContent/MainSectionContent.test.js
+++ src/components/MainSectionContent/MainSectionContent.test.js
@@ -1,13 +1,49 @@
-import products from '../../testData'
+import { products } from '../../testData'
 import MainSectionContent from './MainSectionContent'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  act,
+} from '@testing-library/react'
 import { getProducts } from '../../apiCalls'
 jest.mock('../../apiCalls')
 
 describe('MainSectionContent', () => {
-  it('should render a message if no products are available to display', async () => {
+  it('should fetch products on page load', async () => {
+    const productsFetch = getProducts.mockResolvedValueOnce(products)
     render(<MainSectionContent />)
-    getProducts.mockResolvedValueOnce(products)
+    await act(() => productsFetch)
     await waitFor(() => expect(getProducts).toHaveBeenCalledTimes(1))
+    expect(products).toHaveLength(3)
+    cleanup()
+  })
+
+  it('should display a product on page load', async () => {
+    const productsFetch = getProducts.mockResolvedValueOnce(products)
+    render(<MainSectionContent />)
+    await act(() => productsFetch)
+    const product = await waitFor(() => screen.findByText('iPhone 13 Pro'))
+    await act(() => product)
+    expect(product).toBeInTheDocument()
+    cleanup()
+  })
+
+  it('should display multiple products on page load', async () => {
+    const productsFetch = getProducts.mockResolvedValueOnce(products)
+    render(<MainSectionContent />)
+    await act(() => productsFetch)
+    const product1 = await screen.findByText('iPhone 13 Pro')
+    const product2 = await screen.findByText('Air Pods Pro 2')
+    const product3 = await screen.findByText(
+      'Sony Playstation 5 DualSense Wireless Controller'
+    )
+    expect(product1).toBeInTheDocument()
+    expect(product2).toBeInTheDocument()
+    expect(product3).toBeInTheDocument()
+    cleanup()
+    screen.debug()
   })
 })
