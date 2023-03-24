ac84dc0bf55511d5af7105a13c740c7f024e5304
Horacio Borrego <hborrego365@gmail.com>
2023-03-24 16:58:04 -0700

    Add tests to confirm presence of form inputs, presence of empty form inputs by default, ability to update form input values

--- src/components/MainSectionContent/MainSectionContent.test.js
+++ src/components/MainSectionContent/MainSectionContent.test.js
@@ -62,6 +62,17 @@ describe('MainSectionContent', () => {
     const status = screen.getByText(/choose an option/i)
     const imageUrl = screen.getByLabelText(/image URL/i)
     const imagePreview = screen.getByAltText(/placeholder/i)
+    const nameInput = screen.getByRole('textbox', { name: /name/i })
+    const skuInput = screen.getByRole('textbox', { name: /sku/i })
+    const imageInput = screen.getByRole('textbox', { name: /image url/i })
+    const priceInput = screen.getByRole('spinbutton', { name: /price/i })
+    const quantityInput = screen.getByRole('spinbutton', { name: /quantity/i })
+    const descriptionInput = screen.getByRole('textbox', {
+      name: /description/i,
+    })
+
+    const statusDropDown = screen.getByRole('combobox')
+
     expect(heading).toBeInTheDocument()
     expect(productName).toBeInTheDocument()
     expect(sku).toBeInTheDocument()
@@ -71,6 +82,81 @@ describe('MainSectionContent', () => {
     expect(imageUrl).toBeInTheDocument()
     expect(imagePreview).toBeInTheDocument()
     expect(status).toBeInTheDocument()
+    expect(nameInput).toBeInTheDocument()
+    expect(skuInput).toBeInTheDocument()
+    expect(imageInput).toBeInTheDocument()
+    expect(priceInput).toBeInTheDocument()
+    expect(quantityInput).toBeInTheDocument()
+    expect(descriptionInput).toBeInTheDocument()
+    expect(statusDropDown).toBeInTheDocument()
+    cleanup()
+  })
+
+  it('should not contain form input values when adding a new product', () => {
+    render(<MainSectionContent />)
+    const newProductButton = screen.getByRole('button', {
+      name: /new product/i,
+    })
+    expect(newProductButton).toBeInTheDocument()
+    fireEvent.click(newProductButton)
+    const nameField = screen.getByRole('textbox', { name: /name/i })
+    const skuInput = screen.getByRole('textbox', { name: /sku/i })
+    const imageInput = screen.getByRole('textbox', { name: /image url/i })
+    const priceInput = screen.getByRole('spinbutton', { name: /price/i })
+    const quantityInput = screen.getByRole('spinbutton', { name: /quantity/i })
+    const descriptionInput = screen.getByRole('textbox', {
+      name: /description/i,
+    })
+    const statusDropDown = screen.getByRole('combobox')
+    expect(statusDropDown.value).toBe('')
+    expect(nameField.value).toBe('')
+    expect(skuInput.value).toBe('')
+    expect(imageInput.value).toBe('')
+    expect(priceInput.value).toBe('')
+    expect(quantityInput.value).toBe('')
+    expect(descriptionInput.value).toBe('')
+    cleanup()
+  })
+
+  it('should be able to fill out the product form', () => {
+    render(<MainSectionContent />)
+    const newProductButton = screen.getByRole('button', {
+      name: /new product/i,
+    })
+    expect(newProductButton).toBeInTheDocument()
+    fireEvent.click(newProductButton)
+    const nameField = screen.getByRole('textbox', { name: /name/i })
+    const skuInput = screen.getByRole('textbox', { name: /sku/i })
+    const imageInput = screen.getByRole('textbox', { name: /image url/i })
+    const priceInput = screen.getByRole('spinbutton', { name: /price/i })
+    const quantityInput = screen.getByRole('spinbutton', { name: /quantity/i })
+    const descriptionInput = screen.getByRole('textbox', {
+      name: /description/i,
+    })
+    const statusDropDown = screen.getByRole('combobox')
+
+    fireEvent.change(nameField, { target: { value: 'Ipad Pro' } })
+    fireEvent.change(skuInput, { target: { value: 'XDS4567UOP098' } })
+    fireEvent.change(imageInput, {
+      target: { value: 'https://www.someIpadImage.jpg' },
+    })
+    fireEvent.change(priceInput, { target: { value: '999' } })
+    fireEvent.change(quantityInput, { target: { value: '15' } })
+    fireEvent.change(descriptionInput, {
+      target: { value: 'Ipad Pro 12th generation blah blah blah' },
+    })
+    fireEvent.change(statusDropDown, { target: { value: 'In stock' } })
+    const imagePreviewUpdatedAlt = screen.getByAltText(/Ipad Pro/i)
+    expect(imagePreviewUpdatedAlt).toBeInTheDocument()
+    expect(nameField.value).toBe('Ipad Pro')
+    expect(skuInput.value).toBe('XDS4567UOP098')
+    expect(imageInput.value).toBe('https://www.someIpadImage.jpg')
+    expect(priceInput.value).toBe('999')
+    expect(quantityInput.value).toBe('15')
+    expect(descriptionInput.value).toBe(
+      'Ipad Pro 12th generation blah blah blah'
+    )
+    expect(statusDropDown.value).toBe('In stock')
     cleanup()
   })
 })
